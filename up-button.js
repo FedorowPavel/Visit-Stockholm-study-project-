c4827929a5e07cf6d38cd265455d0d84e48c6165
FedorowPavel <pasha_fedorow@mail.ru>
2020-09-18 15:58:19 +0200

    small changes

--- up-button.js
+++ up-button.js
@@ -1,6 +1,5 @@
 window.addEventListener("scroll", function () {
     let scrollPos = window.scrollY;
-    console.log(scrollPos);
 
     if (scrollPos > 500) {
         document.getElementsByClassName('top-button')[0].style = "opacity: 1";
