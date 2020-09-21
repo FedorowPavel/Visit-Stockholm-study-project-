8f1a0faced3d6c66c58908c08f9baa48e15095e1
FedorowPavel <pasha_fedorow@mail.ru>
2020-09-21 10:42:13 +0200

    entartainment page first update

--- up-button.js
+++ up-button.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function () {
     let scrollPos = window.scrollY;
 
-    if (scrollPos > 500) {
+    if (scrollPos > 250) {
         document.getElementsByClassName('top-button')[0].style = "opacity: 1";
     } else {
         document.getElementsByClassName('top-button')[0].style = "opacity: 0";
